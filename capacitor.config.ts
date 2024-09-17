e896b387fd6a7c6591226287b80ad42f0a1edf4c
Gadelha TI <gadelha.ti@gmail.com>
2024-09-17 03:16:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fla-app',
+  webDir: 'dist'
+};
+
+export default config;
